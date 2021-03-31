b6472bc9bfb407773d550c692e452c7a6faec942
chak88 <88chak88@gmail.com>
2021-03-31 14:41:23 +0300

    important commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
-def get_random(n):
+def get_random(n,s):
     import random
-    return random.randint(0,n)
+    return random.randint(s,n)
 
 if __name__ == '__main__':
     print('Hello Word')
-    get_random(10)
+    get_random(10,20)
